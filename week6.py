32ca9bcbbb290056e61a185ac00487e9c6563890
knownree <108420849+knownree@users.noreply.github.com>
2022-08-08 19:06:58 +0100

    Add files via upload

---
+++ week6.py
@@ -0,0 +1,23 @@
+from modin import pandas as pd
+import dask.dataframe as dd
+import testutility as util
+config_data = util.read_config_file("file.yaml")
+file_type = config_data['file_type']
+source_file = "./" + config_data['file_name'] + f'.{file_type}'
+#print("",source_file)
+df = pd.read_csv(source_file,config_data['inbound_delimiter'])
+df1=dd.read_csv(source_file,dtype='str')
+util.col_header_val(df,config_data)
+print('pd:')
+print("columns of files are:" ,df.columns)
+print('Dask:')
+print("columns of files are:" ,df1.columns)
+print("columns of YAML are:" ,config_data['columns'])
+if util.col_header_val(df,config_data)==0:
+    print("pd:validation failed")
+else:
+    print("pd:col validation passed")
+if util.col_header_val(df,config_data)==0:
+    print("dask:validation failed")
+else:
+    print("dask:col validation passed")
